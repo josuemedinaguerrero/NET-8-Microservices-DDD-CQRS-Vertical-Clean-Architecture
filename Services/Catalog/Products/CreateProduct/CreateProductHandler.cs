429907368823d73418c1be6faaa38ab9f5a69269
Josué Medina Guerrero <josuemedina.032810@gmail.com>
2025-03-01 16:01:47 -0500

    docker-compose added

--- Services/Catalog/Products/CreateProduct/CreateProductHandler.cs
+++ Services/Catalog/Products/CreateProduct/CreateProductHandler.cs
@@ -1,13 +1,10 @@
-using BuildingBlocks.CQRS;
-using Catalog.Models;
-
 namespace Catalog.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
@@ -20,7 +17,10 @@ namespace Catalog.Products.CreateProduct
                 Price = command.Price,
             };
 
-            return new CreateProductResult(Guid.NewGuid());
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(product.Id);
         }
     }
 }
--- Services/Catalog/obj/Debug/net8.0/Catalog.AssemblyInfo.cs
+++ Services/Catalog/obj/Debug/net8.0/Catalog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f248ba65bd328886707814789b47889cbece108")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
