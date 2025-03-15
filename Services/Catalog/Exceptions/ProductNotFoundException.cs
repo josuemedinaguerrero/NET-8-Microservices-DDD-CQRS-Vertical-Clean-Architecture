9f887fa9c599dbc39f183b900869c84fa0a1ff22
Josué Medina Guerrero <josuemedina.032810@gmail.com>
2025-03-15 10:47:27 -0500

    update product and get product by id

---
+++ Services/Catalog/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,10 @@
+namespace Catalog.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException() : base("Product not found!")
+        {
+
+        }
+    }
+}
