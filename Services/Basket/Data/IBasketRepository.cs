da1436b65c1d1fbb80f55ea299ff7b3348804240
Josué Medina Guerrero <josuemedina.032810@gmail.com>
2025-03-23 18:45:15 -0500

    basket service

---
+++ Services/Basket/Data/IBasketRepository.cs
@@ -0,0 +1,9 @@
+namespace Basket.Data
+{
+   public interface IBasketRepository
+   {
+      Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default);
+      Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default);
+      Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default);
+   }
+}
---
+++ Services/Basket/GlobalUsing.cs
@@ -0,0 +1,8 @@
+global using Basket.Models;
+global using BuildingBlocks.CQRS;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using FluentValidation;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions.Handler;
